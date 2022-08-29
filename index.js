91842e258ea13186c5c6576bc15034cf94c295f4
JHasselbring <janmykhail.hasselbring@outlook.com>
2022-08-29 17:18:20 -0500

    init

---
+++ index.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const hexvector = require('hexvector');
+
+const lock = (input, output, offset) => {
+    let readStream = fs.createReadStream(input);
+    let writeStream = fs.createWriteStream(output);
+    let pendingString = '';
+    readStream.on('data', chunk => {
+        let chunkString = chunk.toString('hex');
+        pendingString = '';
+        for (var i = 0; i < chunkString.length; i++) {
+            let newHex = hexvector.lock(chunkString.charAt(i), offset);
+            pendingString = pendingString + newHex;
+        }
+        let chunkBuffer = Buffer.from(pendingString, 'hex');
+        writeStream.write(chunkBuffer, error => {
+            if (error) {
+                console.error(error);
+            }
+        })
+    });
+
+    readStream.on('end', () => {
+        fileReadEnd = true;
+    });
+}
+const unlock = (input, output, offset) => {
+    let readStream = fs.createReadStream(input);
+    let writeStream = fs.createWriteStream(output);
+    let pendingString = '';
+    readStream.on('data', chunk => {
+        let chunkString = chunk.toString('hex');
+        pendingString = '';
+        for (var i = 0; i < chunkString.length; i++) {
+            let newHex = hexvector.unlock(chunkString.charAt(i), offset);
+            pendingString = pendingString + newHex;
+        }
+        let chunkBuffer = Buffer.from(pendingString, 'hex');
+        writeStream.write(chunkBuffer, error => {
+            if (error) {
+                console.error(error);
+            }
+        })
+    });
+
+    readStream.on('end', () => {
+        fileReadEnd = true;
+    });
+}
+
+exports.lock = lock;
+exports.unlock = unlock;
