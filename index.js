af6bf19963058beb7ff02ec128e46b8b7bf59a36
JHasselbring <janmykhail.hasselbring@outlook.com>
2022-10-06 21:44:20 -0500

    Promisify hexvault

--- index.js
+++ index.js
@@ -1,50 +1,92 @@
 const fs = require('fs');
 const hexvector = require('hexvector');
-
-const lock = (input, output, offset, cb) => {
-    let readStream = fs.createReadStream(input);
-    let writeStream = fs.createWriteStream(output);
-    let pendingString = '';
-    readStream.on('data', chunk => {
-        let chunkString = chunk.toString('hex');
-        pendingString = '';
-        for (var i = 0; i < chunkString.length; i++) {
-            let newHex = hexvector.lock(chunkString.charAt(i), offset);
-            pendingString = pendingString + newHex;
-        }
-        let chunkBuffer = Buffer.from(pendingString, 'hex');
-        writeStream.write(chunkBuffer, error => {
-            if (error) {
-                console.error(error);
+const lock = (input, output, offset) => {
+    return new Promise((resolve, reject) => {
+        let readStream = fs.createReadStream(input);
+        let writeStream = fs.createWriteStream(output);
+        let pendingString = '';
+        let ended = false;
+        readStream.on('data', chunk => {
+            let chunkString = chunk.toString('hex');
+            pendingString = '';
+            for (var i = 0; i < chunkString.length; i++) {
+                let newHex = hexvector.lock(chunkString.charAt(i), offset);
+                pendingString = pendingString + newHex;
             }
-        })
-    });
+            let chunkBuffer = Buffer.from(pendingString, 'hex');
+            writeStream.write(chunkBuffer, error => {
+                if (error) {
+                    console.error('Oh no! ', error);
+                }
+                if (ended == true) {
+                    writeStream.end();
+                }
+            })
+        });
 
-    writeStream.on('close', () => {
-        cb();
-    });
+        // Read has ended but don't return yet because write might not be done
+        readStream.on('end', () => {
+            ended = true;
+            resolve({ success: true });
+        });
+        // Reject immediately on error
+        readStream.on('error', () => {
+            writeStream.end();
+            reject({ success: false });
+        })
+
+        writeStream.on('close', () => {
+        });
+        // Reject immediately on error
+        writeStream.on('error', () => {
+            writeStream.end();
+            reject({ success: false });
+        });
+    })
 }
-const unlock = (input, output, offset, cb) => {
-    let readStream = fs.createReadStream(input);
-    let writeStream = fs.createWriteStream(output);
-    let pendingString = '';
-    readStream.on('data', chunk => {
-        let chunkString = chunk.toString('hex');
-        pendingString = '';
-        for (var i = 0; i < chunkString.length; i++) {
-            let newHex = hexvector.unlock(chunkString.charAt(i), offset);
-            pendingString = pendingString + newHex;
-        }
-        let chunkBuffer = Buffer.from(pendingString, 'hex');
-        writeStream.write(chunkBuffer, error => {
-            if (error) {
-                console.error(error);
+const unlock = (input, output, offset) => {
+    return new Promise((resolve, reject) => {
+        let readStream = fs.createReadStream(input);
+        let writeStream = fs.createWriteStream(output);
+        let pendingString = '';
+        let ended = false;
+        readStream.on('data', chunk => {
+            let chunkString = chunk.toString('hex');
+            pendingString = '';
+            for (var i = 0; i < chunkString.length; i++) {
+                let newHex = hexvector.unlock(chunkString.charAt(i), offset);
+                pendingString = pendingString + newHex;
             }
+            let chunkBuffer = Buffer.from(pendingString, 'hex');
+            writeStream.write(chunkBuffer, error => {
+                if (error) {
+                    console.error(error);
+                }
+                if (ended) {
+                    writeStream.end();
+                    
+                }
+            })
+        });
+        readStream.on('end', () => {
+            ended = true;
+            resolve({ success: true });
+        });
+        // Reject immediately on error
+        readStream.on('error', () => {
+            writeStream.end();
+            reject({ success: false });
         })
-    });
-    writeStream.on('close', () => {
-        cb();
-    });
+
+        writeStream.on('close', () => {
+        });
+
+        // Reject immediately on error
+        writeStream.on('error', () => {
+            writeStream.end();
+            reject({ success: false });
+        });
+    })
 }
 
 exports.lock = lock;
